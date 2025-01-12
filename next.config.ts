ef9f89928018794acb08c2f432934210de376a9c
Rumi Maharjan <rumimaharjan99@gmail.com>
2025-01-12 14:34:42 +0545

    image update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost', 'generally-eager-donkey.ngrok-free.app'],
+  },
 };
 
 export default nextConfig;
